1db53589a12de19e8056c27dcddcbde39bddffe2
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-10-23 22:11:06 +0300

    add second screen

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -3,9 +3,39 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+   
+    let users: [UserData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+    }
+    
+    
+    
+    override func shouldPerformSegue(withIdentifier: String, sender: Any?) -> Bool {
+        //        users.forEach { user in
+        //            guard userNameTF.text == user.name
+        //                    passwordTF.text ==  user.password
+        //            else {
+        //                return false
+        //            }
+        //
+        //
+        //        }
+          return true
+    
+    }
 }
 
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+}
---
+++ AboutMeApp/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  AboutMeApp
+//
+//  Created by Anastasya Maximova on 01.10.2024.
+//
+
+import Foundation
+
+struct UserData {
+    var name: String
+    var password: String
+}
