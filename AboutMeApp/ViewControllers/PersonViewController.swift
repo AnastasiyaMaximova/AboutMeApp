121df32ac2604e1415135a2f34b53908c913a7c9
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-11-06 21:41:53 +0300

    setup UI

---
+++ AboutMeApp/ViewControllers/PersonViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BioViewController.swift
+//  AboutMeApp
+//
+//  Created by Anastasya Maximova on 05.11.2024.
+//
+
+import UIKit
+
+final class : UIViewController {
+    
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personSurnameLabel: UILabel!
+    @IBOutlet weak var ProfessionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
